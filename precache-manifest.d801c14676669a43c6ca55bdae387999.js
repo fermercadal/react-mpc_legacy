ab7473d6be8340d35803c52d8097cddbf65ff97e
Fernando Mercadal <fermercadal@outlook.com>
2020-04-27 20:05:22 -0300

    Updates

---
+++ precache-manifest.d801c14676669a43c6ca55bdae387999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f58062b8dbbef9a2c1cf97a08250ee4",
+    "url": "/mpc/index.html"
+  },
+  {
+    "revision": "39c8494f7f548dadd396",
+    "url": "/mpc/static/css/main.fc9e26a5.chunk.css"
+  },
+  {
+    "revision": "a16481d58066f73e077c",
+    "url": "/mpc/static/js/2.edd1bae5.chunk.js"
+  },
+  {
+    "revision": "39c8494f7f548dadd396",
+    "url": "/mpc/static/js/main.0341fab1.chunk.js"
+  },
+  {
+    "revision": "52691247bca3be5ffb6a",
+    "url": "/mpc/static/js/runtime-main.93fe3e22.js"
+  },
+  {
+    "revision": "90e5916b09d9952e2488f985ea103578",
+    "url": "/mpc/static/media/clap.90e5916b.mp3"
+  }
+]);
